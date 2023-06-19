5c79df293f00d58405a94308dedb6b27c24ca614
Aayush <sharmaaayush2310@gmail.com>
2023-06-19 23:44:24 +0530

    Day 2 completed

---
+++ Day 2 Arrays 2/Merge Overlapping Subintervals/Brute Force Solution/approach.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> mergeIntervals(vector<vector<int>>& intervals) 
+{
+    int n = intervals.size();
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> ans;
+
+    for(int i=0;i<n;i++)
+    {
+        int start =intervals[i][0];
+        int end = intervals[i][1];
+
+        if(!ans.empty() && end<=ans.back()[1])
+        {
+            continue ;
+        }
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(intervals[j][0]<=end)
+            {
+                end=max(end, intervals[j][1]);
+            }
+            else{
+                break;
+            }
+        }
+
+        ans.push_back({start, end});
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<vector<int>> intervals = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+    vector<vector<int>> merged = mergeIntervals(intervals);
+
+    cout << "Merged Intervals: ";
+    for (const vector<int>& interval : merged) {
+        cout << "[" << interval[0] << ", " << interval[1] << "] ";
+    }
+    cout << endl;
+
+    return 0;
+}
