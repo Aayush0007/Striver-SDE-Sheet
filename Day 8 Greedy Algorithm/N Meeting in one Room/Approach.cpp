5b0d37460f03b476630a0b18bbe05aa59c5db0d7
Aayush <sharmaaayush2310@gmail.com>
2023-08-03 05:12:13 +0530

    Completed Greedy and recursion

---
+++ Day 8 Greedy Algorithm/N Meeting in one Room/Approach.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct meeting {
+   int start;
+   int end;
+   int pos;
+};
+
+class Solution {
+   public:
+      bool static comparator(struct meeting m1, meeting m2) {
+         if (m1.end < m2.end) return true;
+         else if (m1.end > m2.end) return false;
+         else if (m1.pos < m2.pos) return true;
+         return false;
+      }
+   void maxMeetings(int s[], int e[], int n) {
+      struct meeting meet[n];
+      for (int i = 0; i < n; i++) {
+         meet[i].start = s[i], meet[i].end = e[i], meet[i].pos = i + 1;
+      }
+
+      sort(meet, meet + n, comparator);
+
+      vector < int > answer;
+
+      int limit = meet[0].end;
+      answer.push_back(meet[0].pos);
+
+      for (int i = 1; i < n; i++) {
+         if (meet[i].start > limit) {
+            limit = meet[i].end;
+            answer.push_back(meet[i].pos);
+         }
+      }
+      cout<<"The order in which the meetings will be performed is "<<endl;
+      for (int i = 0; i < answer.size(); i++) {
+         cout << answer[i] << " ";
+      }
+
+   }
+
+};
+int main() {
+   Solution obj;
+   int n = 6;
+   int start[] = {1,3,0,5,8,5};
+   int end[] = {2,4,5,7,9,9};
+   obj.maxMeetings(start, end, n);
+   return 0;
+}
+
+
+/*
+
+bool cmp(pair<int,pair<int,int>> a,pair<int,pair<int,int>> b){
+    if(a.second.first==b.second.first) return a.second.second<b.second.second;
+    return a.second.first<b.second.first;
+    
+}
+
+int maximumActivities(vector<int> &start, vector<int> &finish) {
+    // Write your code here.
+    vector<pair<int,pair<int,int>>> v;
+    for(int i=0;i<start.size();i++){
+        v.push_back({start[i],{finish[i],i+1}});
+    }
+    sort(v.begin(),v.end(),cmp);
+    int l=v[0].second.first;
+    int c=1;
+    for(int i=1;i<start.size();i++){
+        if(v[i].first>=l){
+            //cout<<"k"<<v[i].first<<endl;
+            c++;
+            l=v[i].second.first;
+        }
+    }
+    return c;
+}
+
+*/
